0813488ed54508135d4e968ebf51de6088b32bbf
TatsVals <98864703+TatsVals@users.noreply.github.com>
2022-08-07 12:09:56 -0600

    para merge en master

--- WebApp/wwwroot/App/User/Grid.js
+++ WebApp/wwwroot/App/User/Grid.js
@@ -1,18 +1,26 @@
 "use strict";
 var UserGrid;
 (function (UserGrid) {
-    if (MensajeApp != "") {
-        Toast.fire({ icon: "success", title: MensajeApp });
-    }
-    function OnClickEliminar(id) {
-        ComfirmAlert("Desea eliminar el registro seleccionado?", "Eliminar", "warning", '#3085d6', '#d33')
+    function OnclickEliminar(id) {
+        ComfirmAlert("¿Desea eliminar el registro?", "Eliminar", "warning", '#3085d6', '#d33')
             .then(function (result) {
             if (result.isConfirmed) {
-                window.location.href = "Users/Grid?handler=Eliminar&id=" + id;
+                Loading.fire("Borrando");
+                App.AxiosProvider.DeleteUsers(id).then(function (data) {
+                    Loading.close();
+                    if (data.CodeError == 0) {
+                        Toast.fire({ title: "El registro se elimino correctamente", icon: "success" }).
+                            then(function () { return window.location.reload(); });
+                    }
+                    else {
+                        Toast.fire({ title: data.MsgError, icon: "error" });
+                    }
+                });
             }
         });
     }
-    UserGrid.OnClickEliminar = OnClickEliminar;
+    UserGrid.OnclickEliminar = OnclickEliminar;
+    /*Datable*/
     $("#GridView").DataTable();
 })(UserGrid || (UserGrid = {}));
 //# sourceMappingURL=Grid.js.map
