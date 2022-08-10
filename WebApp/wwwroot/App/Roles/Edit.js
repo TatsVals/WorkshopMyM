d9f3ae2d8345abd6bfd3240b6c3982a8fea623cd
TatsVals <98864703+TatsVals@users.noreply.github.com>
2022-08-09 19:19:35 -0600

    Usando JS

--- WebApp/wwwroot/App/Roles/Edit.js
+++ WebApp/wwwroot/App/Roles/Edit.js
@@ -1,12 +1,35 @@
 "use strict";
 var RolesEdit;
 (function (RolesEdit) {
-    function OnclickGuardar() {
-        Toast.fire({ title: "El registro se elimino correctamente", icon: "success" }).
-            then(function () { return window.location.reload(); });
-    }
-    RolesEdit.OnclickGuardar = OnclickGuardar;
-    /*Datable*/
-    $("#GridView").DataTable();
+    var Entity = $("#AppEdit").data("entity");
+    var Formulario = new Vue({
+        data: {
+            Formulario: "#FormEdit",
+            Entity: Entity
+        },
+        methods: {
+            Save: function () {
+                if (BValidateData(this.Formulario)) {
+                    Loading.fire("Guardando..");
+                    App.AxiosProvider.SaveRoles(this.Entity).then(function (data) {
+                        Loading.close();
+                        if (data.CodeError == 0) {
+                            Toast.fire({ title: "El registro se guardó correctamente", icon: "success" }).then(function () { return window.location.href = "Roles/Grid"; });
+                        }
+                        else {
+                            Toast.fire({ title: data.MsgError, icon: "error" });
+                        }
+                    });
+                }
+                else {
+                    Toast.fire({ title: "Por favor complete los campos requeridos" });
+                }
+            }
+        },
+        mounted: function () {
+            CreateValidator(this.Formulario);
+        }
+    });
+    Formulario.$mount("#AppEdit");
 })(RolesEdit || (RolesEdit = {}));
 //# sourceMappingURL=Edit.js.map
