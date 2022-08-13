37e51f76ac134767f406211aef7cdaf081a54f2a
BryanPeraza36 <bperaza07@hotmail.com>
2022-08-13 09:10:15 -0600

    Triggers Movimientos

---
+++ DataBaseWorkshopMyM/dbo/Triggers/Delete.sql
@@ -0,0 +1,9 @@
+﻿CREATE TRIGGER [Delete]
+	ON dbo.Users 
+	AFTER DELETE
+	AS
+	BEGIN
+		SET NOCOUNT ON
+		INSERT INTO Bitacora_Movimientos (Nombre_Usuario, Fecha, Movimiento, Detalle)
+		VALUES ('QUEMADO', GETDATE(), 'DELETE', 'Usuario eliminado')
+	END
---
+++ DataBaseWorkshopMyM/dbo/Triggers/Update.sql
@@ -0,0 +1,9 @@
+﻿CREATE TRIGGER [Update]
+		ON dbo.Users 
+	AFTER UPDATE
+	AS
+	BEGIN
+		SET NOCOUNT ON
+		INSERT INTO Bitacora_Movimientos (Nombre_Usuario, Fecha, Movimiento, Detalle)
+		VALUES ('QUEMADO', GETDATE(), 'UPDATE', 'Usuario Actualizado')
+	END
