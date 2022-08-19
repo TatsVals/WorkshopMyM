7f17fb80962c89a21a7fa07abb860086ded7c038
TatsVals <98864703+TatsVals@users.noreply.github.com>
2022-08-19 02:09:51 -0600

    s

--- WebApp/wwwroot/App/Bitacora_Movimientos/Grid.js
+++ WebApp/wwwroot/App/Bitacora_Movimientos/Grid.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#GridView thead tr').clone(true).addClass('filters').appendTo('#GridView thead');
+    
     $('#GridView').DataTable({
         language: {
             url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
@@ -43,70 +43,7 @@ $(document).ready(function () {
             //extra
             'pageLength'
         ],
-        //______________ QUINTO _______________
-        orderCellsTop: true,
-        fixedHeader: true,
-        initComplete: function () {
-            var api = this.api();
-
-
-
-            // For each column
-            api.columns().eq(0).each(function (colIdx) {
-
-
-
-                // Set the header cell to contain the input element
-                var cell = $('.filters th').eq($(api.column(colIdx).header()).index());
-
-
-
-                var title = $(cell).text();
-
-
-
-                //$(cell).html('<input type="text" placeholder="' + title + '" />');
-                $(cell).html('<input type="text" placeholder="Buscar..." />');
-
-
-
-                // On every keypress in this input
-                $('input', $('.filters th').eq($(api.column(colIdx).header()).index())).off('keyup change').on('keyup change', function (e) {
-
-
-
-                    e.stopPropagation();
-
-
-
-                    // Get the search value
-                    $(this).attr('title', $(this).val());
-                    var regexr = '({search})'; //$(this).parents('th').find('select').val();
-
-
-
-                    var cursorPosition = this.selectionStart;
-                    // Search the column for that value
-                    api
-                        .column(colIdx)
-                        .search(
-                            this.value != ''
-                                ? regexr.replace('{search}', '(((' + this.value + ')))')
-                                : '',
-                            this.value != '',
-                            this.value == ''
-                        )
-                        .draw();
-
-
-
-                    $(this)
-                        .focus()[0]
-                        .setSelectionRange(cursorPosition, cursorPosition);
-                });
-            });
-        },
-
+        
 
 
         //extra
